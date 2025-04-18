3a50ea432a1cfbbdf926aff4c77f14d1187f776c
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-18 14:03:31 +0300

    Implement EditNoteView and EditNoteViewBody; update NoteItem to navigate to EditNoteView

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,51 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'flutter ddde',
-              style: TextStyle(fontSize: 26, color: Colors.black),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'ddddd sdss ds sddddddd sdss dsddddd',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black.withValues(alpha: .5),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (ctx) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'flutter ddde',
+                style: TextStyle(fontSize: 26, color: Colors.black),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'ddddd sdss ds sddddddd sdss dsddddd',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withValues(alpha: .5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(FontAwesomeIcons.trash, color: Colors.black, size: 24),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              '12/2/2000',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black.withValues(alpha: .4),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                '12/2/2000',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black.withValues(alpha: .4),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
