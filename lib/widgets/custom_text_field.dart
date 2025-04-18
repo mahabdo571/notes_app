588a0bf31bde5cfa688bab2abf24bf31ba72887d
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-18 13:44:20 +0300

    Implement AddNoteBottomSheet layout and create CustomTextField widget for improved note entry

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  final String hint;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(color: kPrimaryColor),
+        enabledBorder: buildBorder(),
+        border: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([Color color = Colors.white]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: color),
+    );
+  }
+}
