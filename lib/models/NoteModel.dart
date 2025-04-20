3565fe76826be414c20dfad54d6e76d4540cb527
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-20 09:51:59 +0300

    Add NoteModel class to define note structure with title, subtitle, date, and color properties

---
+++ lib/models/NoteModel.dart
@@ -0,0 +1,13 @@
+class Notemodel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  Notemodel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
