aac4facb56ba3a71a7d6558f981ddc0ea3b9ac7f
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-21 11:00:08 +0300

    Refactor imports in main.dart, add_note_bottom_sheet.dart, and notes_view_body.dart to clean up unused dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/simple_bloc_observer.dart';
 import 'constants.dart';
-import 'cubits/add_note_cubit/add_note_cubit.dart';
 import 'models/note_model.dart';
 import 'views/notes_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'add_note_form.dart';
 
@@ -19,7 +18,7 @@ class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
       child: BlocConsumer<AddNoteCubit, AddNoteState>(
         listener: (context, state) {
           if (state is AddNoteFailure) {
-            print('errrrrrrrrrrr ${state.errMessage}');
+            
           }
           if (state is AddNoteSuccess) {
             Navigator.pop(context);
@@ -34,7 +33,7 @@ class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
                 left: 16,
                 bottom: MediaQuery.of(context).viewInsets.bottom,
               ),
-              child: SingleChildScrollView(child: AddNoteForm()),
+              child:const SingleChildScrollView(child: AddNoteForm()),
             ),
           );
         },
