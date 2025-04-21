f7d2c3068b929cafdd7a8e977bf499ef7122baeb
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-21 11:09:06 +0300

    Implement NotesCubit and NotesState for state management in notes feature

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+  
+  fetchAllNotes() {
+    var noteBox = Hive.box<NoteModel>(kNoteBox);
+
+    List<NoteModel> notes = noteBox.values.toList();
+  }
+}
