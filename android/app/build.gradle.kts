5ee57179c982babea26860b9a2e6e9541ba6b193
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-21 18:26:41 +0300

    Update versionCode and versionName to 2 and "1.1" respectively

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
     }
 
